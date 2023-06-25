e5ec4157a0003c8d3eddf24b9655580436c349f4
Chocs <Luisitom@gmail.com>
2023-06-24 19:10:59 -0600

    Menu updated

--- Practica2.asm
+++ Practica2.asm
@@ -1,30 +1,78 @@
 .model small
+.RADIX 16
 .stack 
 
 
 .data 
-    msg db 'hello a todos! ',10,13,'$'
     usac       db    "Universidad de San Carlos de Guatemala",0a,"$"
     facultad   db    "Facultad de Ingenieria",0a,"$"
     curso      db    "Arquitectura de Computadoras y Ensambladores 1",0a,"$"
-    nombre     db    "Luis Mariano Moreira Garcia",0a,"$"
-    carne      db    "202010770",0a,"$"
+    nombre     db    "Nombre: Luis Mariano Moreira Garcia",0a,"$"
+    carne      db    "Carnet: 202010770",0a,"$"
+    menu_productos db "(P)roductos", 0a, "$"
+    menu_ventas db "(V)entas", 0a, "$"
+    menu_herramientas db "(H)erramientas", 0a, "$"
+    salto_de_linea db 0a,"$"
 ; segemento de codigo    
 .code
-
-    ; procedimiento principal main
-	main PROC
-
-        ; carga en memoria las variables del semento de datos
     	MOV ax, @data
-    	MOV ds, ax  
+    	MOV ds, ax 
 
-        ; impresion por pantalla
-		mov dx, offset msg
-		mov ah, 9
-    	int 21h   
-    	.exit   
+Header:
+		mov DX, offset usac
+		mov AH, 09
+		int 21
+		mov DX, offset facultad
+		mov AH, 09
+		int 21
+		mov DX, offset curso
+		mov AH, 09
+		int 21
+		mov DX, offset nombre
+		mov AH, 09
+		int 21
+		mov DX, offset carne
+		mov AH, 09
+		int 21
 
-	main ENDP   
+Menu:
+    mov DX, offset menu_productos
+	mov AH, 09
+	int 21
+    mov DX, offset menu_ventas
+	mov AH, 09
+	int 21
+    mov DX, offset menu_herramientas
+	mov AH, 09
+	int 21
 
-end main
+    mov AH, 01
+    int 21
+
+    mov DX, offset salto_de_linea
+	mov AH, 09
+	int 21
+
+    cmp AL, 70
+	je Terminamos
+    cmp AL, 50
+	je Terminamos
+    cmp AL, 56
+	je Terminamos
+    cmp AL, 76
+    je Terminamos
+    cmp AL, 48
+	je Terminamos
+    cmp AL, 68
+    je Terminamos
+    jmp Menu
+
+
+
+Terminamos:
+    .exit
+
+
+
+  
+end 
